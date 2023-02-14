eb56b35aed19a1a67dc74f497465b7aec8481f82
LeinnyJimenez <jimenexleinny@gmail.com>
2023-02-13 20:22:07 -0400

    toggle scene mode

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import data from './data'
 import Article from './Article'
 
 function App() {
+
+  const [theme, setTheme] = useState('light-theme');
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme')
+    } else {
+      setTheme('light-theme')
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme
+  }, [theme])
   return <main>
     <nav>
       <div className='nav-center'>
         <h1>Overreacted</h1>
-        <button className='btn'>toggle scene mode</button>
+        <button className='btn' onClick={toggleTheme}>toggle scene mode</button>
       </div>
     </nav>
     <section className='articles'>
